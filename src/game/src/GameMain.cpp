d21acdcd727ea9436a7617d32cd73da72ffd4c7d
mlnitoon2 <30rwilson@kaufman-isd.net>
2025-02-24 08:33:17 -0600

    Update GameMain.cpp

--- src/game/src/GameMain.cpp
+++ src/game/src/GameMain.cpp
@@ -2,12 +2,12 @@
 #include "../../sys/src/Singleton.h"
 #include <string>
 
-string BBB_AUTH_SERVER = "https://msmpc.bbbgame.net/";
-string BBB_AUTH_MOBILE_SERVER = "https://msm-auth.bbbgame.net/";
+std::string BBB_AUTH_SERVER = "https://msmpc.bbbgame.net/";
+std::string BBB_AUTH_MOBILE_SERVER = "https://msm-auth.bbbgame.net/";
 
 bool MOBILE = false;
 
-string PREGAME_SETUP = BBB_AUTH_SERVER + "pregame_setup.php";
+std::string PREGAME_SETUP = BBB_AUTH_SERVER + "pregame_setup.php";
 int main()
 {
     if (MOBILE == true) {
