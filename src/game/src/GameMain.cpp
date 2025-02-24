2ced831862ee47eff595c9338f37b7c7204476db
mlnitoon2 <30rwilson@kaufman-isd.net>
2025-02-24 08:35:28 -0600

    Update GameMain.cpp

--- src/game/src/GameMain.cpp
+++ src/game/src/GameMain.cpp
@@ -1,6 +1,9 @@
 // GameMain.cpp
 #include "../../sys/src/Singleton.h"
+#include "../../sys/src/HTTPRequest.h"
 #include <string>
+
+string BBB_AUTH2_SERVER = "https://auth.bbbgame.net/auth/api/";
 
 std::string BBB_AUTH_SERVER = "https://msmpc.bbbgame.net/";
 std::string BBB_AUTH_MOBILE_SERVER = "https://msm-auth.bbbgame.net/";
@@ -8,6 +11,25 @@ std::string BBB_AUTH_MOBILE_SERVER = "https://msm-auth.bbbgame.net/";
 bool MOBILE = false;
 
 std::string PREGAME_SETUP = BBB_AUTH_SERVER + "pregame_setup.php";
+
+void BBBAuth2() {
+  // TODO 
+  
+  try
+  {
+      http::Request request{BBB_AUTH2_SERVER + "anon_account/"};
+      const string body = "g=27";
+      const auto response = request.send("POST", body, {
+          {"Content-Type", "application/x-www-form-urlencoded"}
+      });
+      std::cout << std::string{response.body.begin(), response.body.end()} << '\n';
+  }
+  catch (const std::exception& e)
+  {
+      std::cerr << "Request failed, error: " << e.what() << '\n';
+  }  
+}
+
 int main()
 {
     if (MOBILE == true) {
